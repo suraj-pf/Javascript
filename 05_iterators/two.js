4cf9bea711d16b063031de048d5246ee2f21be42
Suraj More <more.suraj.pf@gmail.com>
2025-11-04 09:56:09 +0530

    learnt while do while loop

---
+++ 05_iterators/two.js
@@ -0,0 +1,29 @@
+// // while do-while
+
+// // while (condition) {
+// //     // execution 
+// // }
+
+// // do {
+    
+// // } while (condition);
+
+// let index = 1
+// while (index<=10) {
+//     console.log(index);
+//     index = index + 2 
+// }
+
+// do {
+//     console.log(index);
+//     index++
+// } while (index>10);
+
+let myArray = ['flash','batman','thor','hulk']
+
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+    arr ++ 
+}
